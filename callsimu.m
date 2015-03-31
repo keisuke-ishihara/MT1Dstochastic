a498e36f56779f5c6b31ae2cf67a2b38123ed4f5
tidio634 <tidio634@gmail.com>
2015-03-31 00:35:59 -0400

    subtle diff for Odyssey

--- callsimu.m
+++ callsimu.m
@@ -5,11 +5,11 @@ load('param.mat');
 
 tic
 for i = 1:n_rep
-    
+    cd ~/MT1Dstochastic % comment this line for local test
    	[time, result] = MTsimulation();
 % 	filename = strcat('simu',num2str(i),'.mat');
-	filename = strcat('simu',sprintf('%04.0f',i),'.mat');
-	save(filename);
-    
+	filename = strcat('result',sprintf('%04.0f',i),'.mat');
+    cd(wd); % comment this line for local test
+    save(filename);
 end
 toc
