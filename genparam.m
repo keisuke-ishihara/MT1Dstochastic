52a390d56065491996540bd92f3a4c646bd208d2
tidio634 <tidio634@gmail.com>
2015-03-31 11:22:58 -0400

    genpparam and sbatch
    genparam.m
    added simu_arrays.sbatch

--- genparam.m
+++ genparam.m
@@ -1,40 +1,51 @@
+dirname = '20150331_1';
+mkdir(dirname);
+cd(dirname);
+
 clear all;
 
-workingpath = strcat(fileparts(pwd),'/experiments_MT1Dstoch/20150330_1');
+variation = 0:0.5:2;
 
-global xbin tmax Nmax midpts;
-xbinwidth = 4;              % in microns
-xbin = 0:xbinwidth:200;  
-midpts = (xbin(1:end-1)+xbin(2:end))/2;
-tmax = 20;
-Nmax = 10000;           % max number of MTs to simulate
+for i = 1:length(variation)
+   
+    global xbin tmax Nmax midpts;
+    xbinwidth = 4;              % in microns
+    xbin = 0:xbinwidth:200;  
+    midpts = (xbin(1:end-1)+xbin(2:end))/2;
+    tmax = 20;
+    Nmax = 10000;           % max number of MTs allowed to simulate
 
-global plusendCap mtCap;
-% define carrying capacities so density is robust to xbinwidth
-plusendCap = 2.5*xbinwidth;
-mtCap      = 2.5*xbinwidth;
+    global plusendCap mtCap;
+    % define carrying capacities so density is robust to xbinwidth
+    plusendCap = 2.5*xbinwidth;
+    mtCap      = 2.5*xbinwidth;
 
-global boundarycondition nucscenario depolyreg;
-boundarycondition = 3;
-nucscenario = 2;
-depolyreg = 0;
+    global boundarycondition nucscenario depolyreg;
+    boundarycondition = 3;
+    nucscenario = 2;
+    depolyreg = 0;
 
-global v_poly v_depoly f_cat f_res;
-v_poly   =  8.8;    v_depoly   = 13.7; 
-f_cat = 0.05*60;    f_res = 0.006*60;
+    global v_poly v_depoly f_cat f_res;
+    v_poly   =  8.8;    v_depoly   = 13.7; 
+    f_cat = 0.05*60;    f_res = 0.006*60;
 
-global Ni dt;
-dt = 0.04;
-Ni = 100;
+    global Ni dt;
+    dt = 0.04;          % time step for simulation
+    Ni = 100;           % initial number of immortal microtubules at origin
 
-global nucrate n_rep
-nucrate = 1;
-n_rep = 2;
+    global nucrate n_rep
+    nucrate = variation(i);        % rate of nucleation
+    n_rep = 5;          % how many simulations to repeat per condition
 
-J = (v_poly*f_res - v_depoly*f_cat)/(f_cat+f_res);
-D = v_poly*v_depoly/(f_cat+f_res);
-tau =4*D/J^2;
-L = D/abs(J);
+    J = (v_poly*f_res - v_depoly*f_cat)/(f_cat+f_res);
+    D = v_poly*v_depoly/(f_cat+f_res);
+    tau =4*D/J^2;
+    L = D/abs(J);
 
-save(strcat(workingpath,'/param.mat'));
-cd(workingpath);
+    save(strcat('param',num2str(i),'.mat'));
+
+end
+
+clear all
+
+% cd(workingpath);
