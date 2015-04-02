12c2db947cae791de6bcee2ea000eb4b28230c3c
tidio634 <Keisuke@Keisuke-Ishiharas-iMac.local>
2015-04-02 17:57:48 -0400

    fix simu_arrays

--- callsimu.m
+++ callsimu.m
@@ -7,9 +7,9 @@ for i = 1:n_rep
 
     old = cd(scriptdir);
     
-    nucrate
+    % reset the rng seed and start simulation
+    rng shuffle
     [time, result] = MTsimulation();
-	filename = strcat('result',sprintf('%04.0f',i),'.mat');
 
     % display progress of simulation
     out1 = strcat('Simulation', {' '}, num2str(i), ' out of ', {' '}, num2str(n_rep), '.', {' '}); 
@@ -17,7 +17,7 @@ for i = 1:n_rep
     strcat(out1,out2)
     
     cd(old);
+    filename = strcat('result',sprintf('%04.0f',i),'.mat');
     save(filename);
         
 end
-
--- modoki_simu_arrays.sh
+++ modoki_simu_arrays.sh
@@ -9,21 +9,21 @@ for FILENAME in *.mat; do
     SUBSTRING=`echo $FILENAME| cut -d'.' -f 1`
     
     mkdir ${SUBSTRING}_out
-    cp ${FILENAME} ./param.mat
-#    cp ${FILENAME} ./${SUBSTRING}_out/param.mat
-#    cd ${SUBSTRING}_out
-
-    scriptdir=`pwd |sed 's#/[^/]*/[^/]*$##'`  
-#    scriptdir=`pwd |sed 's#/[^/]*/[^/]*/[^/]*$##'`  
-    echo $scriptdir
-
+#    cp ${FILENAME} ./param.mat
+    cp ${FILENAME} ./${SUBSTRING}_out/param.mat
     cd ${SUBSTRING}_out
+
+#    scriptdir=`pwd |sed 's#/[^/]*/[^/]*$##'`  
+    scriptdir=`pwd |sed 's#/[^/]*/[^/]*/[^/]*$##'`  
+#    echo $scriptdir
+
+#    cd ${SUBSTRING}_out
     
     matlab -nojvm -nosplash -nodesktop < ${scriptdir}/callsimu.m
     
-#    rm param.mat
-    cd ..
     rm param.mat
+    cd ..
+#    rm param.mat
 
 done;
 
